c66d895d0fb2099a79f7bc8c576f9ff99a3d8cec
David <davplanes@gmail.com>
2025-03-18 12:25:15 +0200

    Added floating button that shows/hides the input constraint layout

--- app/src/main/java/com/example/recyclervw_complete/CategoriesActivity.java
+++ app/src/main/java/com/example/recyclervw_complete/CategoriesActivity.java
@@ -1,9 +1,12 @@
 package com.example.recyclervw_complete;
 
+import android.opengl.Visibility;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -14,13 +17,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.davidkerman.viewmodel.CategoriesViewModel;
 import com.example.model.Categories;
 import com.example.model.Category;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CategoriesActivity extends AppCompatActivity {
 
-    private RecyclerView    rvCategories;
-    private Categories      categories;
-    private CategoryAdapter cAdapter;
-    private CategoriesViewModel categoriesViewModel;
+    private RecyclerView         rvCategories;
+    //private Categories           categories;
+    private CategoryAdapter      cAdapter;
+    private CategoriesViewModel  categoriesViewModel;
+    private ConstraintLayout     inputCL;
+    private FloatingActionButton fButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,5 +75,23 @@ public class CategoriesActivity extends AppCompatActivity {
 
     private void initializeViews() {
         rvCategories = findViewById(R.id.rvCategories);
+        fButton = findViewById(R.id.fButton);
+        inputCL = findViewById(R.id.inputCL);
+
+        SetListeners();
+    }
+
+    private void SetListeners() {
+        fButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (inputCL.getVisibility() == View.VISIBLE){
+                    inputCL.setVisibility(View.GONE);
+                }
+                else{
+                    inputCL.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
