abca2aa4c323ff4f7f7834b16ccb5542667ef761
user <davplanes@gmail.com>
2025-03-24 01:05:50 +0200

    Added ToyActivity and initial layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.circleimageview)
     implementation(project(":MODEL"))
     implementation(project(":VIEWMODEL"))
     testImplementation(libs.junit)
